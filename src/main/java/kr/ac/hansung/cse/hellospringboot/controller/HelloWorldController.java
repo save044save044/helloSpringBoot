1e915f32207210507d8092cfbdd4f50a4ef01afa
save044 <save044save044>
2021-05-21 19:38:52 +0900

    add sayhi

--- src/main/java/kr/ac/hansung/cse/hellospringboot/controller/HelloWorldController.java
+++ src/main/java/kr/ac/hansung/cse/hellospringboot/controller/HelloWorldController.java
@@ -10,4 +10,8 @@ public class HelloWorldController {
     public String sayHello(){
         return "index";
     }
+    @GetMapping("/hi")
+    public String sayHi(){
+        return "index";
+    }
 }
